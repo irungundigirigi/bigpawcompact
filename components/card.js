473bf35a2e1e7723b5bc919cc2338dab6bfd8891
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-06-17 13:18:56 +0300

    continuous commits

--- components/card.js
+++ components/card.js
@@ -9,6 +9,13 @@ function Packagecard() {
         <Image className={styles.img} src={pic} />
         <div className={styles.textHolder}> 
           <span className={styles.heading}>3 Days Mara</span>
+          <p className={styles.description}>
+            This is a short safari featuring Maasai Mara National Reserve. 
+            The safari departs Nairobi in the morning and involves a drive down to the the floor of the Rift Valley,
+            making a stop at view point to see the escarpment and the awesome sight of the valley and the features there in. 
+            To arrive Maasai Mara in timefor lunch, followed by a late afternoon game drive that introduces you to
+            Kenyas most popular game sanctuary.
+          </p>
         </div>
     </div>
   )
