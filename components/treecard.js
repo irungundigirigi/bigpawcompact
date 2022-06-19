0dfe49e01a4f626325684e18a7c0a275c9edfe00
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-06-19 14:04:40 +0300

    continuous commits

--- components/treecard.js
+++ components/treecard.js
@@ -13,8 +13,9 @@ function Treecard() {
         <div className={styles.content}>
             <div className={styles.header}>MASAI MARA</div>
             <div className={styles.paragraph}>
-            This is a short safari featuring Maasai Mara National Reserve.
-            The safari departs Nairobi in the morning and involves a drive down to the the floor of the Rift Valley, making a stop at view point to see the escarpment and the awesome sight of the valley and the features there in. To arrive Maasai Mara in timefor lunch, followed by a late afternoon game drive that introduces you to Kenyas most popular game sanctuary.
+             The Masai Mara is not only home to the lions of "Big Cat Diary"
+             but also the temporary residence of the 'Great Wildebeast Migration', the masai Warriors and 
+             home to some of the most luxurious safari lodges in africa.
             </div>
             <div className={styles.bubbles}>
                 <div className={styles.bubble}> 3 days mara</div>
