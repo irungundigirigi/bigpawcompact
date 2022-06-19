b482518615784ebde75e341a96afaa977d748cc9
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-06-19 14:27:13 +0300

    continuous commits

--- components/treecard.js
+++ components/treecard.js
@@ -13,15 +13,15 @@ function Treecard() {
         <div className={styles.content}>
             <div className={styles.header}>MASAI MARA</div>
             <div className={styles.paragraph}>
-             The Masai Mara is not only home to the lions of "Big Cat Diary"
-             but also the temporary residence of the 'Great Wildebeast Migration', the masai Warriors and 
+             The Masai Mara is not only home to the lions of Big Cat Diary
+             but also the temporary residence of the Great Wildebeast Migration, the masai Warriors and 
              home to some of the most luxurious safari lodges in Africa.
             </div>
             <div className={styles.bubbles}>
                 <div className={styles.bubble}> 3 days mara</div>
                 <div className={styles.bubble}> 2 days</div>
                 <div className={styles.bubble}> 2 days 3 nights mara</div>
-                <div className={styles.bubble}> masai mara / l. nakuru</div>
+                <div className={styles.bubble}> Masai Mara / Lake Nakuru</div>
                 <div className={styles.bubble}></div>
             </div>
 
